a20e5f896cc2ccbb97c904a15fb8f1aeddf66fd3
Noe Vazquez P <famvazpom@gmail.com>
2023-10-01 19:34:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://famvazpom.github.io',
+  base: 'https://famvazpom.tech',
   plugins: [react()],
 })
